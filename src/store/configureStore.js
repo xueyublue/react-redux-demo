953efdbe5bbaab6f58f9fa9e0a18f445ea8e5a66
Xue Yu <xueyublue@gmail.com>
2023-01-03 17:17:35 +0800

    adding more comments

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -8,6 +8,7 @@ export default function configureAppStore() {
   // return configureStore({ reducer, middleware: [logger("console"), func] });
 
   // option 2: to register customized middlware by using redux toolkit
+  // redux toolkit bu default providing functionalities to dispatch function actions
   return configureStore({
     reducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger("console")),
