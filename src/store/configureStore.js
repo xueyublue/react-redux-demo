ae106a29c0c455a41db4b007167bdf8e784ffbd0
Xue Yu <xueyublue@gmail.com>
2023-01-05 20:29:25 +0800

    comment out unused code

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import logger from "./middleware/logger";
-import func from "./middleware/func";
+// import func from "./middleware/func";
 
 export default function configureAppStore() {
   // option 1: to register customized middleware
