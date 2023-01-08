83f1b3f4e15ad929df267dd2ec39b9fecae7c8fd
Xue Yu <xueyublue@gmail.com>
2023-01-08 16:45:28 +0800

    Added basic tests

--- 05_ReduxDemo/src/math.spec.js
+++ 05_ReduxDemo/src/math.spec.js
@@ -1,3 +1,13 @@
-it("first test", () => {
-  console.log("jest");
+import { isEven } from "./math";
+
+describe("isEven", () => {
+  it("should return true if given an even number", () => {
+    const result = isEven(2);
+    expect(result).toEqual(true);
+  });
+
+  it("should return false if given an odd number", () => {
+    const result = isEven(3);
+    expect(result).toEqual(false);
+  });
 });
